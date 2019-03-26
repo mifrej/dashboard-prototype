732ab81db977b21ecf7d42b4e1ef6770a5337686
Milosz Frejnik <milosz@frejnik.com>
2019-03-26 23:27:07 +0100

    Tweak packages and some settings

--- next.config.js
+++ next.config.js
@@ -2,21 +2,5 @@ const withTypescript = require('@zeit/next-typescript');
 const withCSS = require('@zeit/next-css');
 
 module.exports = withTypescript(
-  withCSS(
-    {
-      webpack: config => {
-        config.plugins = config.plugins || []
-
-        config.plugins = [
-          ...config.plugins,
-
-          // new Dotenv({
-          //   path: path.join(__dirname, '.env'),
-          //   systemvars: true,
-          // })
-        ]
-        return config
-      }
-    }
-  )
+  withCSS()
 );
--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "postcss-easy-import": {},
     "postcss-preset-env": {
       browsers: ['last 2 versions', 'ie >= 11'],
-      importFrom: "components/theme/postcss-imports.css",
+      importFrom: "src/components/theme/postcss-imports.css",
       stage: 0,
     },
     "postcss-spiffing": {},
