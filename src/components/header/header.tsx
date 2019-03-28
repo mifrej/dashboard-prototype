6d992100fb637c1c46473bb09e47edc2f4e5be3e
Milosz Frejnik <milosz@frejnik.com>
2019-03-28 08:50:33 +0100

    Nprogress for visual effect on page route change

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,16 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Header = () => {
   return (
